51b09d2eba567c053f8013574a74fc850ceaca3a
Pranshu <singhpranshu874@gmail.com>
2026-06-16 00:16:59 +0530

    update

---
+++ DSA/src/moveZeroes.java
@@ -0,0 +1,49 @@
+// move all zero in end of array...
+
+import java.util.*;
+public class moveZeroes {
+        // Function to move zeroes to the end
+        public void moveZeroes(int[] nums) {
+            // Pointer to the first zero
+            int j = -1;
+
+            // Find the first zero
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 0) {
+                    j = i;
+                    break;
+                }
+            }
+
+            // If no zero found, return
+            if (j == -1) return;
+
+            // Start from the next index of first zero
+            for (int i = j + 1; i < nums.length; i++) {
+                // If current element is non-zero
+                if (nums[i] != 0) {
+                    // Swap with nums[j]
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    // Move j to next zero
+                    j++;
+                }
+            }
+        }
+    }
+
+     class test {
+        public static void main(String[] args) {
+            moveZeroes sol = new moveZeroes();
+            int[] nums = {0, 1, 0, 3, 12};
+            sol.moveZeroes(nums);
+
+            // Print the result
+            for (int num : nums) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
