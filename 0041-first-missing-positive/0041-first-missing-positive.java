db4dae960ec57e0fa60d6c1daeb130277e042c42
Pranshu singh <singhpranshu874@gmail.com>
2026-07-31 22:31:02 +0530

    Time: 1 ms (99.96%), Space: 71.4 MB (38.44%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+      
+        int n = nums.length;
+
+        // Put every number at its correct index
+        for (int i = 0; i < n; i++) {
+
+            while (nums[i] >= 1 &&
+                   nums[i] <= n &&
+                   nums[i] != nums[nums[i] - 1]) {
+
+                int correctIndex = nums[i] - 1;
+
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            }
+        }
+
+        // Find first missing positive
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+}
